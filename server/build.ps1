72306370e83c5486cedf4c797aa7e4e34c667694
jakubsepik <jakubsepik@gmail.com>
2024-02-04 20:53:23 +0100

    files cleanup

--- server/build.ps1
+++
@@ -1,5 +0,0 @@
-if (Test-Path ./server.zip) {
-        Remove-Item ./server.zip
-    }
-Get-ChildItem -Path ./ -File | Where-Object { $_.Extension -ne '.env' -and $_.Extension -ne '.ps1' } -outvariable files
-Compress-Archive $files server.zip
